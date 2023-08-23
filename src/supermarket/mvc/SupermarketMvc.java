335adb149efa80873de0eab22b11b5a0ccae7e7b
unknown <dinethsamihiru@gmail.com>
2023-08-23 10:09:34 +0530

    First commit to MVC

---
+++ src/supermarket/mvc/SupermarketMvc.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package supermarket.mvc;
+
+import supermarket.mvc.view.OrderView;
+
+/**
+ *
+ * @author Samihiru
+ */
+public class SupermarketMvc {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       // new CustomerView().setVisible(true);
+        new OrderView().setVisible(true);
+    }
+    
+}
